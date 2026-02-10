5778560db77d8093f3032649c91ffc957fc25587
sudarshan sethi <sethisudarshan98@gmail.com>
2026-02-10 15:17:39 +0530

    wait mechanism

--- playwright_sir/playwright_work.py
+++ playwright_sir/playwright_work.py
@@ -9,6 +9,13 @@ with sync_playwright() as p:
     ###..Wait Mecanism ........
     page.wait_for_timeout(3000) ### playwright Hard wait
     page.wait_for_selector()
+    ##wait_for_selector
+    ##wait_for_timeout
+    ##wait_for_URL
+    ##wait_for_event
+    ##wait_for_function
+    ##wait_for_load_state
+
 
 
     time.sleep(3)
